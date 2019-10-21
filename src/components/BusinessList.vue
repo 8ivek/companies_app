a934981851f49fa05399ec4108d4f9db58a34698
Bivek <bivek.joshi@mercatus.com>
2019-10-21 17:48:59 -0400

    wip business

---
+++ src/components/BusinessList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="business-list">
+    <table>
+      <thead>
+        <tr>
+          <th>Business Name</th>
+          <th>Business Email</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="business in businesses" :key="business.id">
+          <td>{{ business.name }}</td>
+          <td>{{ business.email }}</td>
+          <td>&nbsp;</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'business-list',
+  props: {
+    businesses: Array,
+  },
+};
+</script>
+
+<style scoped></style>
