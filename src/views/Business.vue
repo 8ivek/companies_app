0dc7b7ffa80b78d9a450dfb5a589b6533ec50ddd
8ivek <bivek_j@yahoo.com>
2019-10-25 00:19:35 -0400

    Updated to work for api

--- src/views/Business.vue
+++ src/views/Business.vue
@@ -22,42 +22,68 @@ export default {
   },
   data() {
     return {
-      businesses: [
-        {
-          id: 1,
-          name: 'Samsung Electronics',
-          email: 'infosamsung.ca',
-        },
-        {
-          id: 2,
-          name: 'Xerox Ltd.',
-          email: 'info@xerox.ca',
-        },
-        {
-          id: 3,
-          name: 'World Vision',
-          email: 'info@worldvision.ca',
-        },
-      ],
+      apiurl: 'http://localhost:8000/api',
+      businesses: [],
     };
   },
+  mounted() {
+    this.getBusinesses();
+  },
   methods: {
-    addBusiness(business) {
-      const lastId = this.businesses.length > 0
-        ? this.businesses[this.businesses.length - 1].id : 0;
-      const id = lastId + 1;
-      const newBusiness = { ...business, id };
-      this.businesses = [...this.businesses, newBusiness];
+    async getBusinesses() {
+      try {
+        const response = await fetch(`${this.apiurl}/companies`);
+        const data = await response.json();
+        this.businesses = data.data;
+      } catch (error) {
+        console.error(error);
+      }
     },
-    deleteBusiness(id) {
-      this.businesses = this.businesses.filter(
-        business => business.id !== id,
-      );
+    async addBusiness(business) {
+      try {
+        const response = await fetch(`${this.apiurl}/companies`, {
+          method: 'POST',
+          body: JSON.stringify(business),
+          headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        });
+        const data = await response.json();
+        this.businesses = [...this.businesses, data];
+      } catch (error) {
+        console.error(error);
+      }
     },
-    editBusiness(id, updatedBusiness) {
-      this.businesses = this.businesses.map(
-        business => (business.id === id ? updatedBusiness : business),
-      );
+    async editBusiness(id, updatedBusiness) {
+      try {
+        const response = await fetch(`${this.apiurl}/companies/${id}`, {
+          method: 'PUT',
+          body: JSON.stringify(updatedBusiness),
+          headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        });
+        const data = await response.json();
+        if (data) {
+          this.businesses = this.businesses.map(
+            business => (business.id === id ? updatedBusiness : business),
+          );
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async deleteBusiness(id) {
+      try {
+        const response = await fetch(`${this.apiurl}/companies/${id}`, {
+          method: 'DELETE',
+          headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        });
+        const data = await response.json();
+        if (data.data === 'Company delete successful.') {
+          this.businesses = this.businesses.filter(
+            business => business.id !== id,
+          );
+        }
+      } catch (error) {
+        console.error(error);
+      }
     },
   },
 };
